42531c6ad35187b55a3656c4bc4974d89944d1b7
Kuldeep Paul <kuldeeppaul@MacBook-Air-5.local>
2026-01-15 22:00:01 +0530

    base path fixed

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  basePath: '/bifrost/model-library',
-  assetPrefix: '/bifrost/model-library',
 };
 
 export default nextConfig;
